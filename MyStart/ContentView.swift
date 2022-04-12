cb9dd162c6038f574df41825089eb47686c8de17
co.abu <anizomxoja@gmail.com>
2022-04-12 15:30:38 +0500

    Initial Commit

---
+++ MyStart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyStart
+//
+//  Created by Avaz Mukhitdinov on 12/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyStart/MyStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStartApp.swift
+//  MyStart
+//
+//  Created by Avaz Mukhitdinov on 12/04/22.
+//
+
+import SwiftUI
+
+@main
+struct MyStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
