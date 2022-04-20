3bfdbfd32c28b6c20c35526a8a26cca3385f8aa7
co.abu <anizomxoja@gmail.com>
2022-04-20 17:14:56 +0500

    add 2 animations

--- MyStart/ContentView.swift
+++ MyStart/ContentView.swift
@@ -9,20 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var half = false
-    @State private var dim = false
+    @State var play = 0
     
     var body: some View {
-        
-        Image("eifel_tower").resizable()
-            .scaleEffect(half ? 0.5 : 1.0)
-            .opacity(dim ? 0.2 : 1.0)
-            .animation(.easeInOut(duration: 1.0))
-            .onTapGesture {
-                self.dim.toggle()
-                self.half.toggle()
-                
+        VStack{
+            LottieView(name: "facebook_login", play: $play)
+                .frame(width: 500, height: 450)
+            Button("Play"){
+                self.play += 1
             }
+            LottieView(name: "facebook_like", play: $play)
+                .frame(width: 400, height: 400)
+            Button("Play"){
+                self.play += 1
+            }
+
+        }
     }
 }
 
---
+++ MyStart/JsonFiles/LottieView.swift
@@ -0,0 +1,51 @@
+//
+//  LottieView.swift
+//  MyStart
+//
+//  Created by Avaz Mukhitdinov on 20/04/22.
+//
+
+import Foundation
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    var name: String!
+    @Binding var play:Int
+    
+    var animationView = AnimationView()
+
+    class Coordinator: NSObject {
+        var parent: LottieView
+    
+        init(_ animationView: LottieView) {
+            self.parent = animationView
+            super.init()
+        }
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        let view = UIView()
+
+        animationView.animation = Animation.named(name)
+        animationView.contentMode = .scaleAspectFit
+
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<LottieView>) {
+        animationView.play()
+    }
+}
